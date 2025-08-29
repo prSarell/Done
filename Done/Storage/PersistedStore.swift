e766163df19b0f78b8d976f32e7dde83c929ead3
prSarell <patsarell@gmail.com>
2025-08-29 17:35:44 +1000

    Add PersistedStore JSON storage helper

---
+++ Done/Storage/PersistedStore.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public final class PersistedStore<Item: Codable> {
+    private let fileURL: URL
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    private let queue = DispatchQueue(label: "PersistedStore.\(Item.self)", qos: .utility)
+
+    public init(filename: String) {
+        let docs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        self.fileURL = docs.appendingPathComponent(filename)
+        encoder.outputFormatting = [.withoutEscapingSlashes]
+        encoder.dateEncodingStrategy = .iso8601
+        decoder.dateDecodingStrategy = .iso8601
+    }
+
+    public func load(default value: [Item] = []) -> [Item] {
+        (try? Data(contentsOf: fileURL))
+            .flatMap { try? decoder.decode([Item].self, from: $0) } ?? value
+    }
+
+    public func save(_ items: [Item]) {
+        queue.async {
+            do {
+                let data = try self.encoder.encode(items)
+                try data.write(to: self.fileURL, options: .atomic)
+            } catch {
+                #if DEBUG
+                print("PersistedStore save error:", error)
+                #endif
+            }
+        }
+    }
+}
